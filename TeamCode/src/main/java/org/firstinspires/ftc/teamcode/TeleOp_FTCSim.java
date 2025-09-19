a3c37fcd67eff96bfe34f9603ab34388248df005
ZayyanFTC <155941135+ZayyanFTC@users.noreply.github.com>
2025-09-19 22:05:46 +0700

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_FTCSim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_FTCSim.java
@@ -1,4 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleOp_FTCSim {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+public class TeleOp_FTCSim extends LinearOpMode {
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    private DcMotor shootwheel;
+    private Servo artifactstopper;
+    boolean isShooting;
+
+    public void runOpMode() {
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        shootwheel = hardwareMap.get(DcMotor.class, "shootwheel");
+        artifactstopper = hardwareMap.get(Servo.class, "shootwheel");
+
+        waitForStart();
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        isShooting = false;
+        artifactstopper.setPosition(0.2);
+
+        while(opModeIsActive()) {
+            double leftPower, rightPower;
+            double drive = gamepad1.right_stick_x;
+            double turn = -gamepad1.left_stick_y;
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+        }
+    }
 }
