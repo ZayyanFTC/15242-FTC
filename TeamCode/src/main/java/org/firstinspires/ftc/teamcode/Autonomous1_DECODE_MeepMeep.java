5d1dc7517497a8f1e62775b61ef7a3953b74580d
ZayyanFTC <155941135+ZayyanFTC@users.noreply.github.com>
2025-09-21 08:38:25 +0700

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1_DECODE_MeepMeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1_DECODE_MeepMeep.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Autonomous1_DECODE_MeepMeep", group="STEAMachines_DECODE")
+@Autonomous(name="Autonomous1_DECODE-SM_MeepMeep", group="STEAMachines_DECODE")
 public class Autonomous1_DECODE_MeepMeep extends LinearOpMode {
     @Override
     public void runOpMode() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1_DECODE_SM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1_DECODE_SM.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(name="Autonomous1_DECODE-SM", group="STEAMachines_DECODE")
 public class Autonomous1_DECODE_SM extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive;
     private DcMotor rightDrive;
 
@@ -11,8 +15,27 @@ public class Autonomous1_DECODE_SM extends LinearOpMode {
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
         waitForStart();
-        while(opModeIsActive()) {
-            
+
+        //Move (forward)
+        leftDrive.setPower(-0.7);
+        rightDrive.setPower(0.7);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.7)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
         }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        //Turn (left)
+        leftDrive.setPower(0.3);
+        rightDrive.setPower(0.3);
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds()<0.3) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
 }
